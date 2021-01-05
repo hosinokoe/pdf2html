6ecc0a52b7d385dfd87cf3c3251cbd06cb26fa55
xiao.kai <xiao.kai@maisiteng.com>
2021-01-05 10:03:02 +0800

    1.添加解析时间计算 2.添加部分log输出 3.解决空格问题 4.修复部分bug

--- pdf2html.py
+++ pdf2html.py
@@ -1,7 +1,8 @@
 import glob,os,time,shutil,psutil,subprocess
+from datetime import datetime
 
-# folder="//192.168.18.72/share/04_Application/shared_pdf_2"
-folder="//192.168.18.72/share/04_Application/tmp/shared_pdf_1"
+folder="c:/bin/pdf"
+# folder="//192.168.18.72/share/04_Application/tmp/shared_pdf_1"
 output="c:/bin/pdf"
 
 while True:
@@ -16,8 +17,15 @@ while True:
 	pdf_length=len(pdf_file)
 	pdf_list=pdf_file[0].split("\\")[1]
 	# file_name=os.path.splitext(pdf_list)[0]
-	cmd_p="e:/tools/ADPRP/APDFPR.EXE -batch %s/%s -q" % (folder,pdf_list)
-	command="c:/bin/PDFConvert.exe -i %s/%s -o %s -f html" % (folder,pdf_list,output)
+	#解决空格问题
+	pdf_list_len=len(pdf_list)
+	pdf_list_len2=len(pdf_list.replace(" ",""))
+	if pdf_list_len2 == pdf_list_len:
+		cmd_p="e:/tools/ADPRP/APDFPR.EXE -batch %s/%s -q" % (folder,pdf_list)
+		command="c:/bin/PDFConvert.exe -i %s/%s -o %s -f html" % (folder,pdf_list,output)
+	else:
+		cmd_p='e:/tools/ADPRP/APDFPR.EXE -batch "%s/%s" -q' % (folder,pdf_list)
+		command='c:/bin/PDFConvert.exe -i "%s/%s" -o "%s" -f html' % (folder,pdf_list,output)
 	# os.system(command)
 	sub_p=subprocess.Popen(cmd_p)
 	time.sleep(2)
@@ -31,6 +39,8 @@ while True:
 	print('tets')
 	# time.sleep(20)
 	sub_p.kill()
+	print('开始解析pdf:',pdf_list)
+	start_t=datetime.now()
 	sub_c=subprocess.Popen(command)
 	time.sleep(5)
 
@@ -39,6 +49,7 @@ while True:
 
 	# html_list=html_list=html_file_src[0].split("\\")[1]
 	# 如果pdf没有解析成html，并且acrobat占用内存超过2500m就kill acrobat。解析完了就跳出循环
+	p=1
 	while not html_length:
 		html_file_src=glob.glob("%s/*.html" % output)
 		html_length=len(html_file_src)
@@ -47,41 +58,52 @@ while True:
 		# 	c.kill()
 		for proc in psutil.process_iter():
 			if proc.name() == 'Acrobat.exe':
-				if(proc.memory_info().rss / 1024 /1024 > 2500):
+				if(proc.memory_info().rss / 1024 /1024 > 2000):
+				# 测试使用120
+				# if(proc.memory_info().rss / 1024 /1024 > 120):
 				# if(proc.memory_info().rss > 104857600):
 				# if(proc.memory_info().rss):
 					print(proc.memory_info().rss)
 					sub_c.kill()
-					os.system("taskkill /f /im Acrobat.exe")
+					p=os.system("taskkill /f /im Acrobat.exe")
+		time.sleep(2)
+		if p == 0:
+			break
 		# html_list=html_file_src[0].split("\\")[1]
 		# if html_length > 0:
 		# 	shutil.move("%s/%s" % (output,html_list),"%s/%s" % (folder,html_list))
+	end_t=datetime.now()
+	spend_t=(end_t - start_t).seconds/60
+	print('解析花费: %s 分钟' % spend_t)
 	html_file_src=glob.glob("%s/*.html" % output)
 	html_length=len(html_file_src)
-	html_list=html_file_src[0].split("\\")[1]
-	if html_length > 0:
-		sub_c.kill()
-		shutil.move("%s/%s" % (output,html_list),"%s/%s" % (folder,html_list))
-	
-	#移动文件计1s
-	time.sleep(2)
-	# 如果有html就不进入下一个循环
-	html_file=glob.glob("%s/*.html" % folder)
-	html_length=len(html_file)
-	while html_length:
+	if p !=0:
+		html_list=html_file_src[0].split("\\")[1]
+		if html_length > 0:
+			sub_c.kill()
+			shutil.move("%s/%s" % (output,html_list),"%s/%s" % (folder,html_list))
+		
+		#移动文件计1s
+		time.sleep(2)
+		# 如果有html就不进入下一个循环
 		html_file=glob.glob("%s/*.html" % folder)
 		html_length=len(html_file)
+		while html_length:
+			html_file=glob.glob("%s/*.html" % folder)
+			html_length=len(html_file)
+	else:
+		break
 
-	# time.sleep(2)
 	# 如果pdf名字没变就不进入下一个循环
 	pdf_file_con=glob.glob("%s/*.pdf" % folder)
 	pdf_con_length=len(pdf_file_con)
 	while not pdf_con_length:
 		pdf_file_con=glob.glob("%s/*.pdf" % folder)
 		pdf_con_length=len(pdf_file_con)
-	# pdf_file_con=glob.glob("%s/*.pdf" % folder)
-	# pdf_list_con=pdf_file_con[0].split("\\")[1]
-	# while pdf_list_con == pdf_list:
-	# 	print(1)
-	# 	pdf_file_con=glob.glob("%s/*.pdf" % folder)
-	# 	pdf_list_con=pdf_file_con[0].split("\\")[1]
+	pdf_file_con=glob.glob("%s/*.pdf" % folder)
+	pdf_list_con=pdf_file_con[0].split("\\")[1]
+	while pdf_list_con == pdf_list:
+		print('pdf名字没变,目前还是:',pdf_list_con)
+		pdf_file_con=glob.glob("%s/*.pdf" % folder)
+		pdf_list_con=pdf_file_con[0].split("\\")[1]
+		time.sleep(5)
